3ff45ae24c829480352427afff2bc6c8f145e62a
James McHugh <jdmchugh111@gmail.com>
2024-04-04 20:05:43 -0400

    Added spec test for ConnectFour class

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -1,1 +1,11 @@
-#no test for file yet using runner file to test basic board setup
+require 'spec_helper'
+
+RSpec.describe ConnectFour do
+    describe "#initialize" do
+        it "exists" do
+           new_game = ConnectFour.new
+           
+           expect(new_game).to be_instance_of(ConnectFour)
+        end
+    end
+end
