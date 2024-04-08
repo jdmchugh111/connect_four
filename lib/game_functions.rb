ed52b457ed9f6abea26dfca1c14773bb64e22e17
Garrett Bowman <garrettbowman248@gmail.com>
2024-04-08 17:11:57 -0500

    WIP: broke a lot of things

--- lib/game_functions.rb
+++ lib/game_functions.rb
@@ -4,111 +4,63 @@ class GameFunctions
     def initialize(rows, columns)
         @board = Board.new(rows, columns)
         @turns = 0
-        @a_counter = 6
-        @b_counter = 6
-        @c_counter = 6
-        @d_counter = 6
-        @e_counter = 6
-        @f_counter = 6
-        @g_counter = 6
-        @letter_array = ["A", "B", "C", "D", "E", "F", "G"]
+        # @a_counter = 6
+        # @b_counter = 6
+        # @c_counter = 6
+        # @d_counter = 6
+        # @e_counter = 6
+        # @f_counter = 6
+        # @g_counter = 6
+        # @letter_array = ["A", "B", "C", "D", "E", "F", "G"]
+        counters = Hash[("A".."G").map { |letter| [letter, 6] }]
     end
 
     def take_turn
         if @turns.even?
             player_turn
-        else @turns.odd?
+        else 
             computer_turn
         end
         @turns += 1
     end
 
     def player_turn(player_input)
-        row = row_index(player_input)
         column = column_index(player_input)
-        @board.board_map[row][column] = "X"
+        if column && valid_column?(player_input)
+            row = @counters[player_input] -= 1
+            @board.board_map[row][column] = "X"
+        end
     end
+
+
+    # def player_turn(player_input)
+    #     row = row_index(player_input)
+    #     column = column_index(player_input)
+    #     @board.board_map[row][column] = "X"
+    # end
 
     def computer_turn
-        computer_turn = @letter_array.sample
-        row = row_index(computer_turn)
-        column = column_index(computer_turn)
-        @board.board_map[row][column] = "O"
-        @board.print_board_map
+        computer_choice = @counters.keys.sample
+        player_turn(computer_choice)
+        #leverage player_turn method to place computer piece
     end
 
-    def row_index(player_input)
-        if player_input == "A"
-            @a_counter -= 1
-        elsif player_input == "B"
-            @b_counter -= 1
-        elsif player_input == "C"
-            @c_counter -= 1
-        elsif player_input == "D"
-            @d_counter -= 1
-        elsif player_input == "E"
-            @e_counter -= 1
-        elsif player_input == "F"
-            @f_counter -= 1
-        elsif player_input == "G"
-            @g_counter -= 1
-        else
-            nil
-        end
-    end
+    # def computer_turn
+    #     computer_turn = @letter_array.sample
+    #     row = row_index(computer_turn)
+    #     column = column_index(computer_turn)
+    #     @board.board_map[row][column] = "O"
+    #     @board.print_board_map
+    # end
 
     def column_index(player_input)
-        if player_input == "A"
-            0
-        elsif player_input == "B"
-            1
-        elsif player_input == "C"
-            2
-        elsif player_input == "D"
-            3
-        elsif player_input == "E"
-            4
-        elsif player_input == "F"
-            5
-        elsif player_input == "G"
-            6
-        else
-            nil
-        end
+        columns = {"A" => 0, "B" => 1, "C" => 2, "D" => 3, "E" => 4, "F" => 5, "G" => 6}
+        columns[player_input]
     end
 
-    def valid_column(input)
-        if input == "A"
-            if @a_counter > 0
-                true
-            end
-        elsif input == "B"
-            if @b_counter > 0
-                true
-            end
-        elsif input == "C"
-            if @c_counter > 0
-                true
-            end
-        elsif input == "D"
-            if @d_counter > 0
-                true
-            end
-        elsif input == "E"
-            if @e_counter > 0
-                true
-            end
-        elsif input == "F"
-            if @f_counter > 0
-                true
-            end
-        elsif input == "G"
-            if @g_counter > 0
-                true
-            end
-        else
-            false
-        end
+    def valid_column?(input)
+        @counters[input] && @counters[input] > 0
+        # prevents nil > 0 error
     end
 
     def game_over?
@@ -121,12 +73,10 @@ class GameFunctions
     def restart_game
         @board.reset_board
         @turns = 0
-        @a_counter = 6
-        @b_counter = 6
-        @c_counter = 6
-        @d_counter = 6
-        @e_counter = 6
-        @f_counter = 6
-        @g_counter = 6
+        reset_counters
+    end
+
+    def reset_counters
+        @counters = Hash[("A".."G").map { |letter| [letter, 6] }]
     end
 end
